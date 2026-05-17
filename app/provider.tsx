5cb828439c3a057ed183d52ded80a36b1f2949dc
ZohaibArshad45 <Zohaibfta@gmail.com>
2026-05-17 20:50:51 +0500

    done

--- app/provider.tsx
+++ app/provider.tsx
@@ -1,34 +1,35 @@
-'use client'
-import React, { useState } from 'react'
-import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import { ConvexProvider, ConvexReactClient } from 'convex/react';
-import { AuthContext } from '@/context/AuthContext';
+"use client";
+import React, { useState } from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { AuthContext } from "@/context/AuthContext";
 
-
+//////////////
 const Provider = ({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) => {
-    const [user, setUser] = useState()
-    const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+  const [user, setUser] = useState();
+  const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
-    return (
-        <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!}>
-            <ConvexProvider client={convex}>
-                <AuthContext.Provider value={{user, setUser}}>
-                    <NextThemesProvider
-                        attribute="class"
-                        defaultTheme="light"
-                        enableSystem
-                        disableTransitionOnChange>
-                        <div>{children}</div>
-                    </NextThemesProvider>
-                </AuthContext.Provider>
-            </ConvexProvider>
-        </GoogleOAuthProvider >
-    )
-}
+  return (
+    <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!}>
+      <ConvexProvider client={convex}>
+        <AuthContext.Provider value={{ user, setUser }}>
+          <NextThemesProvider
+            attribute="class"
+            defaultTheme="light"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <div>{children}</div>
+          </NextThemesProvider>
+        </AuthContext.Provider>
+      </ConvexProvider>
+    </GoogleOAuthProvider>
+  );
+};
 
-export default Provider
+export default Provider;
